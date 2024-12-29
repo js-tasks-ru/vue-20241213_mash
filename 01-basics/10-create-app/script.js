3ddb4d98cfdde3e177691bb264c5500395f90313
VolgaMashchytskaya <volga.mashchytskaya@gmail.com>
2024-12-29 16:42:18 +0300

    tasks

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,19 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+    name: 'app',
+    
+    setup() {
+  
+    var day = new Date();
+    
+    return {
+      today: day.toLocaleDateString('ru-RU', { dateStyle: 'long' })
+    }
+  },
+
+  template: `
+  <div>Сегодня {{ today }}</div>`
+});
+
+createApp(App).mount('#app');
